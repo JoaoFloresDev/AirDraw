4b8399303ba9b7c0427e611a9abbafe941cd9f32
joaofloresdev <j199818@dac.unicamp.br>
2023-10-13 23:00:13 -0300

    Exemplo

--- AirDraw/AirDraw/AppDelegate.swift
+++ AirDraw/AirDraw/AppDelegate.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 Joao Flores. All rights reserved.
 //
 import UIKit
+import GoogleMobileAds
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
+
         return true
     }
 
